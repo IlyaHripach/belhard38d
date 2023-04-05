ad0e0ea8fa3ce614291c5179609b2be32dc04f43
ilya <ilya.hripatch@mail.ru>
2023-04-05 17:00:20 +0300

    add print

---
+++ Lesson 3.py
@@ -0,0 +1,30 @@
+#1
+text = 'Hello world python'
+text1 = text.replace(' ', '-')
+print(text1)
+
+
+
+#2
+
+
+
+
+#3
+name = (input('Введите имя = '))
+age = int(input('Введите возраст = '))
+city = input('Введите город = ')
+print(f'Привет меня зовут {name},  мне {age} лет, я из города {city}')
+
+
+name = (input('Введите имя = '))
+print ('Privet', name)
+age = int(input('Введите возраст = '))
+print ('Супер, теперь введи свой город',name)
+city = input('Введите город = ')
+print('Привет меня зовут', name , 'мне' ,age , 'лет' , 'я из города', city)
+
+
+
+
+ #4
