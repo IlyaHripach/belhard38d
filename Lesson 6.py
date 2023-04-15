ac168927ec05957dc67608e86f5b05deeb06c9cc
ilya <ilya.hripatch@mail.ru>
2023-04-15 12:36:21 +0300

    add print

---
+++ Lesson 6.py
@@ -0,0 +1,39 @@
+#1
+#n = 5 #count
+# m = 13 #division
+# k = 100 #start
+# while n:
+#     if not k % m:
+#         print(k)
+#         n -= 1
+#     k += 1
+
+
+
+#2
+# number1 = (int(input('Введите первое число - ')))
+# action = (input('Выберите действие +, -,  *,  / '))
+# number2 = (int(input('Введите второе число - ')))
+# otvet = ' Ответ = '
+# if action == '+':
+#     print(otvet, number1 + number2)
+# elif action == '-':
+#     print(otvet, number1 - number2)
+# elif action == '*':
+#     print(otvet, number1 * number2)
+# elif action == '/':
+#     print(otvet, number1 / number2)
+# else:
+#      'Введите новый оператор'
+
+
+#3
+# n = 44
+# c = 0
+# for i in range(2, n+1, 2):
+#     print(i, end=' ')
+#     c += 1
+#     if c == 5:
+#         c=0
+#         print()
+
